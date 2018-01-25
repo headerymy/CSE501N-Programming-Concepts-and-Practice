02ab4555ce41675dc3b6d7adf673be80aac97066
Mingyu Yang <headerymy1993@gmail.com>
2018-01-24 20:17:11 -0600

    Add files via upload

---
+++ STUDIOs/studio1/HiFour.java
@@ -0,0 +1,22 @@
+package studio1;
+
+import cse131.ArgsProcessor;
+
+/**
+ * From Sedgewick and Wayne, COS 126 course at Princeton
+ * 
+ */
+public class HiFour {
+	public static void main(String[] args) {
+		ArgsProcessor ap = new ArgsProcessor(args);
+		String s0 = ap.nextString("Enter the first name: ");
+		String s1 = ap.nextString("Enter the second name:");
+		String s2 = ap.nextString("Enter the third name:");
+		String s3 = ap.nextString("Enter the fourth name:");
+		//
+		// Say hello to the names in s0 through s3.
+		//
+		System.out.println("Greetings " + s0 + ", " + s1 + ", " + s2 + ", " + s3 +".");
+
+	}
+}
